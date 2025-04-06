abb8b91d9fc74cf0cc3586122fa71bf6672efb0c
Mehar-glitch-ss <shaikmehartaj04@gmail.com>
2025-04-06 10:21:26 +0530

    changes

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,3 +142,4 @@ STATICFILES_DIRS = [BASE_DIR / 'static',
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
